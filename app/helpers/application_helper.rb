d21b063ae685b7d2456ae4230577924716ff07f9
Evan Derby <somekidwithhtml@gmail.com>
2015-08-21 21:04:01 -0600

    Added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 require 'digest/sha1'
 
 module ApplicationHelper
+  def full_title(title)
+    base_title = "Ellipses Magazine"
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end
+
   def check_password(pw)
   		master_pw_digest = Password.first.password_digest.to_s
   		given_pw_digest = Digest::SHA1.hexdigest pw.to_s
